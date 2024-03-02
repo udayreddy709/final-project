4d9b8eb1bfaa4b60e75bee85f4c4ed0f249b1f81
udayreddy709 <udayreddy709@gmail.com>
2024-03-02 11:02:49 +0530

    finalproject added

---
+++ FinalProject/src/test/java/org/jsp/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
