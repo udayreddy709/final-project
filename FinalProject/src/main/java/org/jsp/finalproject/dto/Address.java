83ad22da094ef8e74ce6fd717d221dff2971adad
skshakeela <sshakeela540@gmail.com>
2024-03-02 11:51:45 +0530

    added dto

---
+++ FinalProject/src/main/java/org/jsp/finalproject/dto/Address.java
@@ -0,0 +1,81 @@
+package org.jsp.finalproject.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+	@Id
+	@GeneratedValue
+	(strategy = GenerationType.IDENTITY)
+private int id;
+	@Column(nullable=false)
+private String street;
+	@Column(nullable=false)
+private String city;
+	@Column(nullable=false)
+private String landmark;
+	@Column(nullable=false)
+private String building_name;
+	@Column(nullable=false)
+private String state;
+	@Column(nullable=false)
+private String country;
+	@Column(nullable=false)
+private int pincode;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getLandmark() {
+		return landmark;
+	}
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+	public String getBuilding_name() {
+		return building_name;
+	}
+	public void setBuilding_name(String building_name) {
+		this.building_name = building_name;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+	
+}
+
---
+++ FinalProject/src/main/java/org/jsp/finalproject/dto/Branch.java
@@ -0,0 +1,46 @@
+package org.jsp.finalproject.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Branch {
+	@Id
+	@GeneratedValue
+	(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(nullable=false)
+	private String name;
+	@Column(nullable=false)
+	private long phone;
+	@Column(nullable=false)
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
+
