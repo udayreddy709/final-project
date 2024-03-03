fc2121bef640ebd4d04328d47f858418f66fb056
skshakeela <sshakeela540@gmail.com>
2024-03-03 23:56:38 +0530

    repository package added

--- FinalProject/src/main/java/org/jsp/finalproject/repository/AddressRepository.java
+++ FinalProject/src/main/java/org/jsp/finalproject/repository/AddressRepository.java
@@ -1,5 +1,27 @@
 package org.jsp.finalproject.repository;
 
-public interface AddressRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.jsp.finalproject.dto.Address;
+import org.jsp.finalproject.dto.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface AddressRepository extends JpaRepository<Branch, Integer> {
+	@Query("select a from Address a where a.city=?1 and b.pincode=?2")
+	public Optional<Address> verify(String city, int pincode);
+	
+	@Query("select b from Address b where b.street=?1")
+	public List<Address> findByStreet(String street);
+
+	
+	public Address save(Address address);
+	
+	@Query("select b from Address b where b.city=?1")
+	public List<Address> findByCity(String city);
+	@Query("select b from Address b where b.country=?1")
+	public List<Address> findByCountry(String country);
+
+	public void delete(Address address);
 }
